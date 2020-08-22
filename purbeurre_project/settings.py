4585a6ce46b274eb3c6848b0c71e2aefb117ab28
raoofrachidi <43079354+raoofrachidi@users.noreply.github.com>
2020-08-22 12:16:18 +0200

    Update settings.py

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -24,10 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rkv1+ujxaa^t&%y2^ag1$g0(!q2*1404zn+d54%$y!)1d8u+(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = ['purbeurre69.herokuapp.com', '127.0.0.1']
 
